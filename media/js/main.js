6c0457fabbe8bd95bccf4da65cccdf2d707f41ac
Mstislav Zhivodkov <mz@wearewowagency.com>
2014-03-28 14:28:13 +0700

    done

--- media/js/main.js
+++ media/js/main.js
@@ -28,50 +28,53 @@ $(function() {
 
     action1 = cal.addAction({
         dateStart: new Date(2014, 02, 26, 9, 5),
-        duration: 55
+        duration: 55,
+        html: '+7 999 999 9999<br/>SibAcadem'
     });
 
     action2 = cal.addAction({
         dateStart: new Date(2014, 02, 26, 10, 32),
         duration: 115,
         addClass: 'busy',
-        html: '<div>Busy</div>'
+        html: '+7 999 999 9999<br/>SibAcadem'
     });
 
     action3 = cal.addAction({
         dateStart: new Date(2014, 02, 25, 10, 00),
-        duration: 15,
+        duration: 25,
         onClick: function() {
             alert('click!');
-        }
+        },
+        html: '+7 999 999 9999<br/>SibAcadem'
     });
 
     action4 = cal.addAction({
         dateStart: new Date(2014, 02, 30, 14, 00),
-        duration: 25
+        duration: 25,
+        html: '+7 999 999 9999<br/>SibAcadem'
     });
 
-
-    cal2 = new gCalendar({
-        timeInterval: 30,
-        parent: $('#calendar-wrap2'),
-        data: dataArray,
-        timeBounds: {
-            start: new gCalendar.Time(12, 00),
-            end: new gCalendar.Time(20, 00)
-        },
-        firstDay: new Date(2014, 02, 25),
-        daysLength: 10,
-        maxScroll: 5
+    cal.addAction({
+        dateStart: new Date(2014, 02, 25, 14, 30),
+        duration: 58,
+        addClass: 'admin',
+        html: '+7 999 999 9999<br/>SibAcadem'
     });
-
-    a1 = cal2.addAction({
-        dateStart: new Date(2014, 02, 30, 14, 00),
-        duration: 50
+    cal.addAction({
+        dateStart: new Date(2014, 02, 27, 11, 30),
+        duration: 120,
+        addClass: 'admin',
+        html: '+7 999 999 9999<br/>SibAcadem'
     });
-
-    a2 = cal2.addAction({
-        dateStart: new Date(2014, 02, 26, 12, 00),
-        duration: 350
+    cal.addAction({
+        dateStart: new Date(2014, 02, 28, 11, 00),
+        duration: 90,
+        html: '+7 999 999 9999<br/>SibAcadem'
+    });
+    cal.addAction({
+        dateStart: new Date(2014, 02, 29, 8, 00),
+        duration: 120,
+        addClass: 'busy',
+        html: '+7 999 999 9999<br/>SibAcadem'
     });
 });
